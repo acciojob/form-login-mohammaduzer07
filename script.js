8b9470d1c90b94bd8ad307a0c558b8577401569c
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-19 16:15:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.querySelector('#fname');
+	let lname = document.querySelector('#lname');
+	let submit = document.querySelector('#submit');
+
+	let first = fname.value;
+	let last = lname.value;
+	
+	// submit.addEventListner("click", fullName);
+	alert(first+" "+last);
+		
+	// let fullName = "";
+	// for(let i=0; i<name.length; i++){
+	// 	if(name.elements[i].value != 'Submit'){
+	// 		fullName += name.elements[i].value;
+	// 	}
+	// }
 
 }
